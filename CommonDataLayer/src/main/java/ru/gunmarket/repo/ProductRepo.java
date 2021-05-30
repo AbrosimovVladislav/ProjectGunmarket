318053cf69cda56b6098054dee2abd8a728fc9e1
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2021-05-30 14:34:02 +0300

    PG-X: check this shit

--- CommonDataLayer/src/main/java/ru/gunmarket/repo/ProductRepo.java
+++ CommonDataLayer/src/main/java/ru/gunmarket/repo/ProductRepo.java
@@ -1,14 +1,19 @@
 package ru.gunmarket.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.gunmarket.model.Brand;
 import ru.gunmarket.model.Product;
 import ru.gunmarket.model.Type;
 import ru.gunmarket.querybuilder.FilterAndSortingRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepo extends JpaRepository<Product, Long>, FilterAndSortingRepository<Product> {
+
+    Optional<Product> findByProductId(Long productId);
+
     default void saveOrUpdate(Product product) {
         if (product.getName().isBlank()) return;
         findByBrandAndNameAndType(product.getBrand(), product.getName(), product.getType())
